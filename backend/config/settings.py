d46a44505ce99b69c8a3d57847c0c725920142ff
Sadhik76 <sadiqultimate76@gmail.com>
2026-01-19 04:58:26 +0530

    Fix ALLOWED_HOSTS for production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ulm7lz%gpzm4(s5folu%c)ahl&qrx@##*2-$+_c$s=h&8l6qu(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
